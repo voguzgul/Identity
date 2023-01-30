8e2140fa9f13d4898a8d287c7396260de770db8c
voguzgul <v.oguzgul@gmail.com>
2023-01-30 21:05:26 +0300

    Projeyi Ayağa Kaldırma

--- IdentityWEB/Program.cs
+++ IdentityWEB/Program.cs
@@ -2,20 +2,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+//builder.Services.AddMvc();//projenin ayaða kalkamsý için tüm bileþenleri kurara.
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
+
+
+
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-app.UseStaticFiles();
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//}
+
+//middleware alaný
+app.UseDeveloperExceptionPage();//sayfada bir hata aldýðýmýzda sayfayla ilgili hatalarý bize sunar. middlewaredir
+app.UseStatusCodePages();//herhangi bir içerik dönemyen sayfalarda bizi bilgilendirici sayfaalrd dönüyor.
+app.UseStaticFiles();//js , css gibi dosyalarýn eklenmesinde kullanýlýr.
+
+
+
+//bu alanda System.InvalidOperationException: 'Endpoint Routing does not support 'IApplicationBuilder.UseMvc(...) hatasý alýrsanýz yukarýda ,
+//builder.Services.AddMvc yerine builder.Services.AddMvc(option => option.EnableEndpointRouting = false); yaz.
+app.UseMvcWithDefaultRoute();//arka planda Controller/Action/{id}  default bir route kullandýrýr.
 
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
 
 app.Run();
